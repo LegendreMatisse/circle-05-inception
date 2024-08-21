849f12c935c143d024f86dd54164df4bf8bf5839
Matisse Legendre <legendre.matisse@gmail.com>
2024-08-22 00:30:46 +0200

    Update startup.sh

--- srcs/requirements/mariadb/tools/startup.sh
+++ srcs/requirements/mariadb/tools/startup.sh
@@ -10,7 +10,7 @@ sleep 1
 echo "CREATE DATABASE IF NOT EXISTS ${DB_NAME} ;" > db.sql
 echo "CREATE USER IF NOT EXISTS '${ADM_USER}'@'%' IDENTIFIED BY '${ADM_PASS}' ;" >> db.sql
 echo "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${ADM_USER}'@'%' ;" >> db.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASS} ;" >> db.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASS}' ;" >> db.sql
 echo "FLUSH PRIVILEGES ;" >> db.sql
 
 mysql < db.sql
